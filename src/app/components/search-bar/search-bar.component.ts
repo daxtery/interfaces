27d4bb1efaaf834b67b6e826506706aa1f84c5c0
David Pinto <m44094@alunos.uevora.pt>
2020-05-27 19:26:54 +0100

    let's make search-bar go to base address

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter, OnDestroy, Input } from '@angular/core
 import { Subject, BehaviorSubject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { DatabaseService } from 'src/app/services/database.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,18 +10,16 @@ import { DatabaseService } from 'src/app/services/database.service';
   styleUrls: ['./search-bar.component.css']
 })
 
-export class SearchBarComponent implements OnDestroy {
+export class SearchBarComponent {
   @Input() readonly placeholder: string = '';
 
   search: string;
 
-  constructor(private service: DatabaseService) {
-  }
-
-  ngOnDestroy() {
+  constructor(private service: DatabaseService, private route: ActivatedRoute, private router: Router) {
   }
 
   public updateSearch() {
+    this.router.navigate(['']);
     this.service.searchedInBox(this.search);
   }
 
