1b34602d957eaa271699b2da5a219d3812e42a5c
David Pinto <m44094@alunos.uevora.pt>
2020-06-09 17:43:09 +0100

    Use constructor public syntax

--- src/app/item.ts
+++ src/app/item.ts
@@ -1,28 +1,12 @@
 export class Item {
-    id: number;
-    name: string;
-    brand: string;
-    categoryNames: string[];
-    unitaryPrice: number;
-    unitaryWeight: number;
-    weightType: string;
-    pictureUrl: string;
 
-    constructor(id: number,
-                name: string,
-                brand: string,
-                categoryNames: string[],
-                unitaryPrice: number,
-                unitaryWeight: number,
-                weightType: string,
-                pictureUrl: string) {
-        this.id = id;
-        this.name = name;
-        this.brand = brand;
-        this.categoryNames = categoryNames;
-        this.unitaryPrice = unitaryPrice;
-        this.unitaryWeight = unitaryWeight;
-        this.weightType = weightType;
-        this.pictureUrl = pictureUrl;
-    }
+    constructor(public id: number,
+                public name: string,
+                public brand: string,
+                public categoryNames: string[],
+                public unitaryPrice: number,
+                public unitaryWeight: number,
+                public weightType: string,
+                public pictureUrl: string,
+                public calories: number) { }
 }
--- src/app/itemView.ts
+++ src/app/itemView.ts
@@ -2,10 +2,16 @@ import { Item } from './item';
 import { Category } from './category';
 
 export class ItemView extends Item {
-    category: Category;
 
-    constructor(item: Item, category: Category) {
-        super(item.id, item.name, item.brand, item.categoryNames, item.unitaryPrice, item.unitaryWeight, item.weightType, item.pictureUrl);
-        this.category = category;
+    constructor(item: Item, public category: Category) {
+        super(item.id,
+            item.name,
+            item.brand,
+            item.categoryNames,
+            item.unitaryPrice,
+            item.unitaryWeight,
+            item.weightType,
+            item.pictureUrl,
+            item.calories);
     }
 }
